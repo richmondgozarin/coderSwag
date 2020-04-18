5edc4e2d15d2fce6245decc166b85b1d72598251
rich gozarin <gozarra@mfcgd.com>
2020-04-18 21:09:30 +0800

    List View Adapter

--- app/src/main/java/com/example/gozarra/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/gozarra/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.example.gozarra.coderswag.Model
 
-class Category(val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
